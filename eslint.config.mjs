8e2e80584026e7d314b6d6263d3b25d0fc4fff9d
adrianstephens <adrianstephens@msn.com>
2025-11-03 09:31:20 -0800

    refactor: update ESLint configuration and add lint script to package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,34 @@
-// @ts-check
 import eslint from "@eslint/js";
 import tslint from "typescript-eslint";
+async function maybeImport(modulePath) { try { return await import(modulePath); } catch { return undefined; } }
+let customPlugin = await maybeImport('../eslint-custom.mjs');
+// @ts-check
 
 export default tslint.config(
-    eslint.configs.recommended,
-    ...tslint.configs.recommended,
-    ...tslint.configs.stylistic,
-    {
-        rules: {
-            "semi": ["error", "always"], // Add this line to enforce semicolon use
-            //"@typescript-eslint/no-misleading-character-class": "off",
-            "@typescript-eslint/no-this-alias": "off",
-            "@typescript-eslint/no-unused-vars":  [
-                "warn", {
-                    argsIgnorePattern: "^(_+$|_[^_])",
-                    varsIgnorePattern: "^(_+$|_[^_])",
-                },
-            ],
-            "@typescript-eslint/no-explicit-any": "off",
-            //"@typescript-eslint/explicit-module-boundary-types": "off",
-            //"@typescript-eslint/no-non-null-assertion": "off"
-	        "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/consistent-indexed-object-style": "off"
+	eslint.configs.recommended,
+	...tslint.configs.recommended,
+	...tslint.configs.stylistic,
+	...(customPlugin ? [customPlugin.config] : []),
+	{
+		rules: {
+			"semi": ["error", "always"], // Add this line to enforce semicolon use
+			//"@typescript-eslint/no-misleading-character-class": "off",
+			"@typescript-eslint/no-this-alias": "off",
+			"@typescript-eslint/no-unused-vars":  [
+				"warn", {
+					argsIgnorePattern: "^(_+$|_[^_])",
+					varsIgnorePattern: "^(_+$|_[^_])",
+				},
+			],
+			"@typescript-eslint/no-explicit-any": "off",
+			//"@typescript-eslint/explicit-module-boundary-types": "off",
+			//"@typescript-eslint/no-non-null-assertion": "off"
+			"@typescript-eslint/no-empty-function": "off",
+			"@typescript-eslint/consistent-indexed-object-style": "off"
 
-        },
-    },
-    {
-        files: ["src/*.ts"],
-    }
+		},
+	},
+	{
+		files: ["src/*.ts"],
+	}
 );
